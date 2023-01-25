0cb604bc8415b513bdfa2bc27a5fc880750f86f3
seregey gornih <you@example.com>
2023-01-25 21:02:08 +0300

    fix client

---
+++ client/server.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const path = require('path')
+const https = require('https');
+const fs = require('fs');
+
+const PORT = process.env.PORT || 3000
+
+const app = express()
+app.use(express.static(__dirname))
+app.use(express.static(path.resolve(__dirname, 'build')))
+
+// Certificate
+const privateKey = fs.readFileSync('privkey.pem', 'utf8'); //fs.readFileSync('/etc/letsencrypt/live/proj.uley.team/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('cert.pem', 'utf8'); //fs.readFileSync('/etc/letsencrypt/live/proj.uley.team/cert.pem', 'utf8');
+const ca = fs.readFileSync('chain.pem', 'utf8'); //fs.readFileSync('/etc/letsencrypt/live/proj.uley.team/chain.pem', 'utf8');
+
+const credentials = {
+    key: privateKey,
+    cert: certificate,
+    ca: ca
+};
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'))
+})
+
+const httpsServer = https.createServer(credentials, app);
+
+const start = async () => {
+    try {       
+        httpsServer.listen(PORT, () => {
+            console.log('HTTPS Server AdminClient running on port ' + PORT);
+        });
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start()
+
