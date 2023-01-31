0594200dffe35cd0b4430e32d7a2c3a74363d138
Sergey <seregaland@yandex.ru>
2023-01-31 16:33:55 +0300

    fix server socket

--- socket/socket_app.js
+++ socket/socket_app.js
@@ -1,3 +1,6 @@
+const fs = require('fs');
+const https = require('https');
+
 // Создание сервера
 const express = require("express");
 const socket = require("socket.io");
@@ -7,20 +10,32 @@ const PORT = 9000;
 const app = express();
 app.use(cors());
 
-const server = app.listen(PORT, function () {
+// Certificate
+const privateKey = fs.readFileSync('privkey.pem', 'utf8'); //fs.readFileSync('/etc/letsencrypt/live/proj.uley.team/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('cert.pem', 'utf8'); //fs.readFileSync('/etc/letsencrypt/live/proj.uley.team/cert.pem', 'utf8');
+const ca = fs.readFileSync('chain.pem', 'utf8'); //fs.readFileSync('/etc/letsencrypt/live/proj.uley.team/chain.pem', 'utf8');
+
+const credentials = {
+    key: privateKey,
+    cert: certificate,
+    ca: ca
+};
+
+const httpsServer = https.createServer(credentials, app);
+
+// const server = app.listen(PORT, function () {
+//     console.log(`Listening on port ${PORT}`);
+//     console.log(`http://proj.uley.team:${PORT}`);
+//   });
+
+httpsServer.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
     console.log(`http://proj.uley.team:${PORT}`);
-  });
+});
 
 // Socket setup
-const io = socket(server);
+const io = socket(httpsServer);
 
-
-// const io = require("socket.io")(9000, {
-//     cors:{
-//         origin:"http://localhost:3000"
-//     },
-// });
 
 let users = [];
 
