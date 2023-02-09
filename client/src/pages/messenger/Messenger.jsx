a1d3b539c55606a8f95d0be58ce1750c25f84cc5
Sergey <seregaland@yandex.ru>
2023-02-09 16:30:17 +0300

    fix array

--- client/src/pages/messenger/Messenger.jsx
+++ client/src/pages/messenger/Messenger.jsx
@@ -60,7 +60,7 @@ export default function Messenger() {
             const getConversations = async () => {
                 try {
                   const res = await $host.get("api/conversations/" + chatAdminId);
-                  setConversations(res.data);
+                  setConversations([res.data, {id: 4, members: [data.senderId, chatAdminId], createdAt: '', updatedAt: ''}]);
                 } catch (err) {
                   console.log(err);
                 }
@@ -69,7 +69,7 @@ export default function Messenger() {
               getConversations();
               //console.log("res: ", res)
             
-            setConversations([...conversations, {id: 4, members: [data.senderId, chatAdminId], createdAt: '', updatedAt: ''} ])
+            //setConversations([...conversations, {id: 4, members: [data.senderId, chatAdminId], createdAt: '', updatedAt: ''} ])
 
             setArrivalMessage({
                 sender: data.senderId,
