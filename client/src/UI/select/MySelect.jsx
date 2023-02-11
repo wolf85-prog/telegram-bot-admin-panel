abf06ce6899af205bb8ec4e8c42c625875fe39fc
seregey gornih <you@example.com>
2023-02-11 21:23:33 +0300

    add select

--- client/src/UI/select/MySelect.jsx
+++ client/src/UI/select/MySelect.jsx
@@ -1,17 +1,19 @@
+/* eslint-disable react/prop-types */
+
 import React from 'react';
 
 const MySelect = (p) => {
     return (
         <select 
-            value={p.value}
-            onChange={event => p.onChange(event.target.value)}
+            // value={value}
+            // onChange={event => onChange(event.target.value)}
         >
-            <option value="">{p.defaultValue}</option>
-            {p.options.map(option =>
+            {/* <option value="">{defaultValue}</option>
+            {options.map(option =>
                 <option key={option.value} value={option.value}>
                     {option.name}
                 </option>
-            )}
+            )} */}
 
         </select>
     )
