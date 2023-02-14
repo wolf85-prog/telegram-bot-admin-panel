d7965c35198f9d30f2ee1d3bf597e4d7ee2029e8
Sergey <seregaland@yandex.ru>
2023-02-14 11:28:10 +0300

    fix chat page

--- client/src/pages/Chat.js
+++ client/src/pages/Chat.js
@@ -1,11 +1,7 @@
-import React, { Suspense } from 'react'
+import React from 'react'
 import Messenger from "./../chat-app/components/Messenger";
 import AccountProvider from './../chat-app/context/AccountProvider';
-import { GoogleOAuthProvider } from '@react-oauth/google';
 import './../chat-app/style/index.css';
-
-// routes config
-import routes from '../routes'
 
 const Chat = () => {
     return (        
