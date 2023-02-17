a16fda38758945b4f364ef7e66c93dabd1a313b3
Sergey <seregaland@yandex.ru>
2023-02-17 17:05:43 +0300

    fix

--- client/src/pages/Chat.js
+++ client/src/pages/Chat.js
@@ -2,12 +2,13 @@ import React from 'react'
 import Messenger from "./../chat-app/components/Messenger";
 import './../chat-app/style/index.css';
 import { AppHeaderChat } from '../components/index'
+import { CContainer, CSpinner } from '@coreui/react'
 
 const Chat = () => {
     return (  
         <div>
             <AppHeaderChat />
-            <Messenger />
+            <Messenger /> 
         </div>              
     );
 }
