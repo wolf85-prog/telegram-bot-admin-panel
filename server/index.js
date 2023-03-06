baca82a3312d11051c4bf23ccea3cbdde41c18af
Sergey <seregaland@yandex.ru>
2023-03-06 12:24:55 +0300

    fix

--- server/index.js
+++ server/index.js
@@ -40,7 +40,7 @@ const credentials = {
 const httpsServer = https.createServer(credentials, app);
 
 // Обработка ошибок, последний middleware
-app.use(errorHandler)
+//app.use(errorHandler)
 
 const start = async () => {
     try {
