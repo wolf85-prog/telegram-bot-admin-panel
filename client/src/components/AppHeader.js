a9f0722cf9f48041f130b87c88d2b9fec7712cad
Sergey <seregaland@yandex.ru>
2023-03-06 12:11:16 +0300

    fix logo

--- client/src/components/AppHeader.js
+++ client/src/components/AppHeader.js
@@ -32,7 +32,8 @@ const AppHeader = () => {
           <CIcon icon={cilMenu} size="lg" />
         </CHeaderToggler>
         <CHeaderBrand className="mx-auto d-md-none" to="/">
-          <CIcon icon={logo} height={48} alt="Logo" />
+          {/* <CIcon icon={logo} height={48} alt="Logo" /> */}
+          <h5>U.L.E.Y TEAM</h5>
         </CHeaderBrand>
         <CHeaderNav className="d-none d-md-flex me-auto">
           <CNavItem>
--- client/src/components/index.js
+++ client/src/components/index.js
@@ -2,7 +2,6 @@ import AppBreadcrumb from './AppBreadcrumb'
 import AppContent from './AppContent'
 import AppFooter from './AppFooter'
 import AppHeader from './AppHeader'
-import AppHeaderChat from './AppHeaderChat'
 import AppHeaderDropdown from './header/AppHeaderDropdown'
 import AppSidebar from './AppSidebar'
 import DocsCallout from './DocsCallout'
@@ -14,7 +13,6 @@ export {
   AppContent,
   AppFooter,
   AppHeader,
-  AppHeaderChat,
   AppHeaderDropdown,
   AppSidebar,
   DocsCallout,
