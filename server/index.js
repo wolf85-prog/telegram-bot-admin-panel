7151450cc67cef73bc73db96ce455e723198f4d3
Sergey <seregaland@yandex.ru>
2023-03-09 15:27:25 +0300

    fix static

--- server/index.js
+++ server/index.js
@@ -19,8 +19,8 @@ app.use(cors())
 app.use(express.json())
 app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true}));
-app.use(express.static(path.resolve(__dirname, 'static')))
 app.use(express.static(path.resolve(__dirname, 'images')))
+app.use('/images', express.static(path.join(__dirname, 'images')))
 
 app.use('/api', Route);
 
