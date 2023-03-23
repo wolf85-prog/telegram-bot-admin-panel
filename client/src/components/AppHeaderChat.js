1c1294bba5f69eebe6024e41603c313673100a6c
Sergey <seregaland@yandex.ru>
2023-03-23 13:34:52 +0300

    fix height

--- client/src/components/AppHeaderChat.js
+++ client/src/components/AppHeaderChat.js
@@ -21,7 +21,7 @@ const AppHeader = () => {
   const sidebarShow = useSelector((state) => state.sidebarShow)
 
   return (
-    <CHeader position="sticky" className="mb-4">
+    <CHeader position="sticky" >
       <CContainer fluid>
         <CHeaderToggler
           className="ps-1"
