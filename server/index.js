4db5e7c912ccb9dc73cc57e1ffb975cbf0e7ed1f
seregey gornih <you@example.com>
2023-03-29 21:16:10 +0300

    fix route

--- server/index.js
+++ server/index.js
@@ -19,9 +19,9 @@ app.use(cors())
 app.use(express.json())
 app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true}));
-app.use(express.static(path.resolve(__dirname, 'images')))
-app.use(express.static(path.resolve(__dirname, 'files')))
-//app.use('/images', express.static(path.join(__dirname, 'images')))
+//app.use(express.static(path.resolve(__dirname, 'images')))
+//app.use(express.static(path.resolve(__dirname, 'files')))
+app.use('/images', express.static(path.join(__dirname, 'images')))
 
 app.use('/api', Route);
 
