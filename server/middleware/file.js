60465124cab2d10fa7ed678c40e1395e96e957b2
Sergey <seregaland@yandex.ru>
2023-04-03 15:38:57 +0300

    fix

--- server/middleware/file.js
+++ server/middleware/file.js
@@ -22,7 +22,7 @@ const storage = multer.diskStorage({
     },
     //замена оригинального названия файла на название текущей даты в миллесекундах
     filename(req, file, cb) {
-        strRegexp = strRegexp.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "_")
+        strRegexp = originalname.match(/[-[\]{}()*+?.,\\^$|#\s]/g)
         const filename = new Date().toISOString() + '-' + file.strRegexp //Date.now()
         cb(null, filename)
     }
