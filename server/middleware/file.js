76e4c0f54639299254753d8c2ad6d4c3150962d4
Sergey <seregaland@yandex.ru>
2023-04-04 16:55:57 +0300

    fix

--- server/middleware/file.js
+++ server/middleware/file.js
@@ -30,6 +30,37 @@ const rusToLat = function(str) {
     }).join('');
 }
 
+function translit(word){
+	var answer = '';
+	var converter = {
+		'а': 'a',    'б': 'b',    'в': 'v',    'г': 'g',    'д': 'd',
+		'е': 'e',    'ё': 'e',    'ж': 'zh',   'з': 'z',    'и': 'i',
+		'й': 'y',    'к': 'k',    'л': 'l',    'м': 'm',    'н': 'n',
+		'о': 'o',    'п': 'p',    'р': 'r',    'с': 's',    'т': 't',
+		'у': 'u',    'ф': 'f',    'х': 'h',    'ц': 'c',    'ч': 'ch',
+		'ш': 'sh',   'щ': 'sch',  'ь': '',     'ы': 'y',    'ъ': '',
+		'э': 'e',    'ю': 'yu',   'я': 'ya',
+ 
+		'А': 'A',    'Б': 'B',    'В': 'V',    'Г': 'G',    'Д': 'D',
+		'Е': 'E',    'Ё': 'E',    'Ж': 'Zh',   'З': 'Z',    'И': 'I',
+		'Й': 'Y',    'К': 'K',    'Л': 'L',    'М': 'M',    'Н': 'N',
+		'О': 'O',    'П': 'P',    'Р': 'R',    'С': 'S',    'Т': 'T',
+		'У': 'U',    'Ф': 'F',    'Х': 'H',    'Ц': 'C',    'Ч': 'Ch',
+		'Ш': 'Sh',   'Щ': 'Sch',  'Ь': '',     'Ы': 'Y',    'Ъ': '',
+		'Э': 'E',    'Ю': 'Yu',   'Я': 'Ya'
+	};
+ 
+	for (var i = 0; i < word.length; ++i ) {
+		if (converter[word[i]] == undefined){
+			answer += word[i];
+		} else {
+			answer += converter[word[i]];
+		}
+	}
+ 
+	return answer;
+}
+
 const storage = multer.diskStorage({
     destination(req, file, cd) {
         
@@ -39,7 +70,10 @@ const storage = multer.diskStorage({
     //замена оригинального названия файла на название текущей даты в миллесекундах
     filename(req, file, cb) {
         //rusToLat(file.originalname.replace(/\s+/g, '')
-        const filename = new Date().toISOString() + '-' + file.originalname //Date.now()
+        const str_file = translit(file.originalname)
+        console.log(file.originalname)
+        console.log(str_file)
+        const filename = new Date().toISOString() + '-' + str_file //Date.now()
         cb(null, filename)
     }
 })
