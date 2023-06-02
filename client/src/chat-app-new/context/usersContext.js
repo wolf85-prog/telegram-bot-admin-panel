c2c829731ce26df908db3b06697432c00f387e6f
Sergey <seregaland@yandex.ru>
2023-06-02 17:54:41 +0300

    fix

--- client/src/chat-app-new/context/usersContext.js
+++ client/src/chat-app-new/context/usersContext.js
@@ -378,22 +378,24 @@ const UsersProvider = ({ children }) => {
 			let userIndex = users.findIndex((user) => user.chatId === senderId.toString());
 			const usersCopy = JSON.parse(JSON.stringify(users));			
 
-			// const newUser = {
-			// 	id: 999,
-			// 	name: 'Новый заказчик',
-			// 	chatId: senderId,
-			// 	avatar: '',
-			// 	conversationId: convId,
-			// 	unread: 0, 
-			// 	pinned: false,
-			// 	typing: false,
-			// 	message:  '',
-			// 	date: '2000-01-01T00:00:00',
-			// 	messages: {}, 
-			// }	
-			// usersCopy.push(newUser)
+			if (!userIndex) {
+				const newUser = {
+					id: usersCopy.length,
+					name: 'Новый заказчик',
+					chatId: senderId,
+					avatar: '',
+					conversationId: convId,
+					unread: 0, 
+					pinned: false,
+					typing: false,
+					message:  '',
+					date: '2000-01-01T00:00:00',
+					messages: {}, 
+				}	
+				usersCopy.push(newUser)
+			}		
 
-			//let userIndex = usersCopy.findIndex((user) => user.chatId === senderId.toString());
+			userIndex = usersCopy.findIndex((user) => user.chatId === senderId.toString());
 //-----------------------------------------------------------------------------------------------			
 			const newMsgObject = {
 				date: new Date().toLocaleDateString(),
