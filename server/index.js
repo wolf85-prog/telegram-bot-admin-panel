0294b29bb8b61c12b9d5029e9d93f08a21c9667b
seregey gornih <you@example.com>
2023-09-04 16:32:51 +0300

    fix

--- server/index.js
+++ server/index.js
@@ -51,6 +51,166 @@ const httpsServer = https.createServer(credentials, app);
 // Обработка ошибок, последний middleware
 app.use(errorHandler)
 
+const getDistributionsPlan = async() => {
+    console.log("Обновляю план рассылок...")
+
+    const d = new Date();
+    const month = String(d.getMonth()+1).padStart(2, "0");
+    const day = String(d.getDate()).padStart(2, "0");
+    const date_str = `${day}.${month}`;
+    
+    d.setDate(d.getDate() + 1);
+    const month2 = String(d.getMonth()+1).padStart(2, "0");
+    const day2 = String(d.getDate()).padStart(2, "0");
+    const date_str2 = `${day2}.${month2}`;
+    const year = d.getFullYear();
+
+    //удаление таймеров
+    console.log("Запускаю очистку задач...")
+    console.log("tasks: ", tasks)
+    tasks.forEach((tmp)=> {
+        clearTimeout(tmp)
+        console.log("Задача удалена! ", tmp)   
+    })
+
+    console.log("Запускаю планировщик задач...")
+
+    //получить запланированные рассылки
+    const distributions = await Distributionw.findAll({
+        order: [
+            ['id', 'ASC'],
+        ],
+        where: {
+            delivered: false
+        }
+    })
+
+    //console.log("Рассылки:", distributions)
+
+    //рассылки
+    distributions.forEach(async (item, index)=> {
+        const date1 = item.datestart //дата отправки рассылки
+        const dateNow = new Date().getTime() + 10800000 //текущая дата
+        console.log("date1: ", new Date(date1))
+        console.log("dateNow: ", new Date(dateNow))
+
+        const d = new Date(date1);
+        const month = String(d.getMonth()+1).padStart(2, "0");
+        const day = String(d.getDate()).padStart(2, "0");
+        const year = d.getFullYear();
+        const chas = d.getHours();
+        const minut = String(d.getMinutes()).padStart(2, "0");
+        const date2 = `${day}.${month}.${year}`
+
+        const milliseconds = Math.floor(new Date(date1) - new Date(dateNow));       
+        console.log("milliseconds: ", milliseconds)
+
+        if (milliseconds > 0) {          
+            const objPlan = {
+                users: item.users.split(','),
+                text: item.text,
+                textButton: item.textButton,
+                image: item.image,
+                time: milliseconds,
+                id: item.id,  
+                projId: item.projectId, 
+                uuid: item.uuid     
+            }
+
+            console.log("!!!!Планирую запуск отправки собщения..." + (index+1))
+            const timerId = setTimeout(() => {
+                objPlan.users.map(async (user, ind) => {
+                    console.log("Пользователю ID: " + user + " сообщение " + item.text + " отправлено! Кнопка " + item.textButton + " отправлена!")
+
+                    //получить план из БД
+                    const plan = await Plan.findOne({
+                        where: {datestart: date2}
+                    })
+                    
+                    const newArray = JSON.parse(plan.dataValues.times)
+                    let time1 = `${chas}:${minut}`
+
+                    //обновить план в БД
+                    let planer_str
+                    let dateIndex = newArray.findIndex((i) => i.time === time1)
+                    const datesCopy = JSON.parse(JSON.stringify(newArray));
+                    const dateObject = datesCopy[dateIndex];
+                    datesCopy[dateIndex] = { ...dateObject, ['go']: true};
+                    planer_str = JSON.stringify(datesCopy)
+
+                    const newObj = {
+                        "datestart": date2,
+                        "times": planer_str
+                    }
+
+                    //обновить план в БД
+                    const foundItem = await Plan.findOne({ where: {datestart: newObj.datestart} });
+                    if (!foundItem) {
+                        // Item not found, create a new one
+                        const newPlan = await Plan.create(newObj.datestart, newObj.times)
+                        //return res.status(200).json(newPlan);
+                    } else {
+                       // Found an item, update it
+                        const item = await Plan.update({times: newObj.times},{where: {datestart: newObj.datestart}});
+                    }
+                    
+
+                    //получить id специалиста по его telegramId
+                    //const worker = await getWorkerId(user)
+                    const worker = await fetch(host_api_bottest + '/workers/chat/' + user);
+                    //console.log(host_api_bottest + '/workers/chat/' + user)
+                    console.log("worker: ", worker)
+                    
+                    //новый претендент
+                    const pretendent = await Pretendent.create(item.projectId, worker.data, user) //{projectId, workerId, receiverId})
+            
+                    //Передаем данные боту
+                    const keyboard = JSON.stringify({
+                        inline_keyboard: [
+                            [
+                                {"text": item.textButton, callback_data:'/report'},
+                            ],
+                        ]
+                    });
+                
+                    const keyboard2 = JSON.stringify({
+                        inline_keyboard: [
+                            [
+                                {"text": 'Принять', callback_data:'/accept ' + pretendent.id}, //  + pretendent.id
+                                {"text": 'Отклонить', callback_data:'/cancel'},
+                            ],
+                        ]
+                    });
+
+                    //отправить в телеграмм
+                    let sendToTelegram
+                    if (item.text !== '') {
+                        const url_send_msg = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${user}&parse_mode=html&text=${item.text.replace(/\n/g, '%0A')}`
+                        
+                        sendToTelegram = await fetch(url_send_msg);
+
+                        const delivered = true
+
+                        //обновить рассылке статус отправки
+                        let exist = await Distributionw.findOne( {where: {id: item.id}} )
+            
+                        if(!exist){
+                            console.log("Рассылка не существует!");
+                        }
+            
+                        const newDistrib = await Distributionw.update(
+                            { delivered },
+                            { where: {id: item.id} }
+                        )
+                    }
+                })
+            }, milliseconds)
+
+            tasks.push(timerId)
+        } 
+    })
+}
+
 const start = async () => {
     try {
         await sequelize.authenticate()
@@ -59,166 +219,11 @@ const start = async () => {
         httpsServer.listen(port, async() => {
             console.log('HTTPS Server Admin-panel running on port ' + port);
 
-            const d = new Date();
-            const month = String(d.getMonth()+1).padStart(2, "0");
-            const day = String(d.getDate()).padStart(2, "0");
-            const date_str = `${day}.${month}`;
-            
-            d.setDate(d.getDate() + 1);
-            const month2 = String(d.getMonth()+1).padStart(2, "0");
-            const day2 = String(d.getDate()).padStart(2, "0");
-            const date_str2 = `${day2}.${month2}`;
-            const year = d.getFullYear();
-
-            //удаление таймеров
-            console.log("Запускаю очистку задач...")
-            console.log("tasks: ", tasks)
-            tasks.forEach((tmp)=> {
-                clearTimeout(tmp)
-                console.log("Задача удалена! ", tmp)   
+            // начало цикла           
+            setInterval(async() => {              
+                getDistributionsPlan()
             })
-
-            console.log("Запускаю планировщик задач...")
-
-            //получить запланированные рассылки
-            const distributions = await Distributionw.findAll({
-                order: [
-                    ['id', 'ASC'],
-                ],
-                where: {
-                    delivered: false
-                }
-            })
-
-            //рассылки
-            distributions.forEach(async (item, index)=> {
-                const date1 = item.datestart //дата отправки рассылки
-                const dateNow = new Date().getTime() + 10800000 //текущая дата
-                console.log("date1: ", new Date(date1))
-                console.log("dateNow: ", new Date(dateNow))
-
-                const d = new Date(date1);
-                const month = String(d.getMonth()+1).padStart(2, "0");
-                const day = String(d.getDate()).padStart(2, "0");
-                const year = d.getFullYear();
-                const chas = d.getHours();
-                const minut = String(d.getMinutes()).padStart(2, "0");
-                const date2 = `${day}.${month}.${year}`
-
-                const milliseconds = Math.floor(new Date(date1) - new Date(dateNow));       
-                console.log("milliseconds: ", milliseconds)
-
-                if (milliseconds > 0) {          
-                    const objPlan = {
-                        users: item.users.split(','),
-                        text: item.text,
-                        textButton: item.textButton,
-                        image: item.image,
-                        time: milliseconds,
-                        id: item.id,  
-                        projId: item.projectId, 
-                        uuid: item.uuid     
-                    }
-
-                    console.log("!!!!Планирую запуск отправки собщения..." + (index+1))
-                    const timerId = setTimeout(() => {
-                        objPlan.users.map(async (user, ind) => {
-                            console.log("Пользователю ID: " + user + " сообщение " + item.text + " отправлено! Кнопка " + item.textButton + " отправлена!")
-
-                            //получить план из БД
-                            const plan = await Plan.findOne({
-                                where: {datestart: date2}
-                            })
-                            
-                            const newArray = JSON.parse(plan.dataValues.times)
-                            let time1 = `${chas}:${minut}`
-
-                            //обновить план в БД
-                            let planer_str
-                            let dateIndex = newArray.findIndex((i) => i.time === time1)
-                            const datesCopy = JSON.parse(JSON.stringify(newArray));
-                            const dateObject = datesCopy[dateIndex];
-                            datesCopy[dateIndex] = { ...dateObject, ['go']: true};
-                            planer_str = JSON.stringify(datesCopy)
-
-                            const newObj = {
-                                "datestart": date2,
-                                "times": planer_str
-                            }
-
-                            //обновить план в БД
-                            const foundItem = await Plan.findOne({ where: {datestart: newObj.datestart} });
-                            if (!foundItem) {
-                                // Item not found, create a new one
-                                const newPlan = await Plan.create(newObj.datestart, newObj.times)
-                                //return res.status(200).json(newPlan);
-                            } else {
-                               // Found an item, update it
-                                const item = await Plan.update({times: newObj.times},{where: {datestart: newObj.datestart}});
-                            }
-                            
-
-                            //получить id специалиста по его telegramId
-                            //const worker = await getWorkerId(user)
-                            const worker = await fetch(host_api_bottest + '/workers/chat/' + user);
-                            //console.log(host_api_bottest + '/workers/chat/' + user)
-                            console.log("worker: ", worker)
-                            
-                            //новый претендент
-                            const pretendent = await Pretendent.create(item.projectId, worker.data, user) //{projectId, workerId, receiverId})
-                    
-                            //Передаем данные боту
-                            const keyboard = JSON.stringify({
-                                inline_keyboard: [
-                                    [
-                                        {"text": item.textButton, callback_data:'/report'},
-                                    ],
-                                ]
-                            });
-                        
-                            const keyboard2 = JSON.stringify({
-                                inline_keyboard: [
-                                    [
-                                        {"text": 'Принять', callback_data:'/accept ' + pretendent.id}, //  + pretendent.id
-                                        {"text": 'Отклонить', callback_data:'/cancel'},
-                                    ],
-                                ]
-                            });
-
-                            //отправить в телеграмм
-                            let sendToTelegram
-                            if (item.text !== '') {
-                                const url_send_msg = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${user}&parse_mode=html&text=${item.text.replace(/\n/g, '%0A')}`
-                                
-                                sendToTelegram = await fetch(url_send_msg);
-
-                                const delivered = true
-
-                                //обновить рассылке статус отправки
-                                let exist = await Distributionw.findOne( {where: {id: item.id}} )
-                    
-                                if(!exist){
-                                    console.log("Рассылка не существует!");
-                                }
-                    
-                                const newDistrib = await Distributionw.update(
-                                    { delivered },
-                                    { where: {id: item.id} }
-                                )
-                            }
-                        })
-                    }, 10000)
-
-                    // const obj = {
-                    //     task: timerId,
-                    //     projectId: item.projectId
-                    // }
-
-                    tasks.push(timerId)
-                } 
-            })
-
-        });
+        }, 120000); //каждые 2 минуты);
 
     } catch (error) {
         console.log('Подключение к БД сломалось!', error)
