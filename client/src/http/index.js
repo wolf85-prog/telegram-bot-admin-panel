4bef0ad8f457a2f8201402df2c10f2ee555b2168
seregey gornih <you@example.com>
2023-09-05 18:42:49 +0300

    fix

--- client/src/http/index.js
+++ client/src/http/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const $host = axios.create({
-    baseURL: process.env.REACT_APP_HOST
+    baseURL: process.env.REACT_APP_API_URL
 })
 
 const $host_bot = axios.create({
