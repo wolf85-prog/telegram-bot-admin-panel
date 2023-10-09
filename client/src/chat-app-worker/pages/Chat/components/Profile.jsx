9360b441e7732d8643639f97be8c59f5f2230974
Sergey <seregaland@yandex.ru>
2023-10-09 17:08:04 +0300

    fix

--- client/src/chat-app-worker/pages/Chat/components/Profile.jsx
+++ client/src/chat-app-worker/pages/Chat/components/Profile.jsx
@@ -123,8 +123,8 @@ const Profile = ({ user }) => {
 				<div className="profile__avatar-wrapper upload">
 					{
 						user?.avatar
-							? <img src={user?.avatar} alt={user?.name} className="avatar-adm" /> //<img src={`${host}${user.avatar}`} alt={user?.name} className="avatar-adm" />
-							: <img src={defaultAvatar} alt={user?.name} className="avatar-adm" />
+							? <figure className="avatar-adm-profile"><img src={user?.avatar} alt={user?.name} /></figure> //<img src={`${host}${user.avatar}`} alt={user?.name} className="avatar-adm" />
+							: <figure className="avatar-adm-profile"><img src={defaultAvatar} alt={user?.name} /></figure>
 					}
 					
 					
