bc39d726a721f7cbf79b74f1bb12e0c6eb4e5da3
seregey gornih <you@example.com>
2023-10-10 21:20:08 +0300

    fix

--- client/src/pages/Admin.js
+++ client/src/pages/Admin.js
@@ -45,6 +45,114 @@ const Admin = () => {
   const host = process.env.REACT_APP_API_URL
   
 
+  //get Contacts
+  useEffect(() => {
+    const arrClients = []
+
+    const fetchData = async() => {
+
+      //let companys = await getCompanys()
+      console.log("companys (admin): ", comps)
+      console.log("clients (admin): ", clients)
+      console.log("managers (admin): ", zakazchiki)
+
+      let messages = await getAllMessages()
+      console.log("messages: ", messages)
+
+      clients.map((client, index) => {
+        
+        const managers = [...zakazchiki];
+        let userIndex = zakazchiki.findIndex((manager) => manager.tgID === client.chatId);  
+        const userObject = managers[userIndex];
+
+        let userObject2 = comps.find((company) => company.managers.find(man => man.id ===  userObject?.id)) //company.managers.map((manager) => manager.id === userObject?.id));  
+
+        const companyName = userObject2?.title
+        const companyCity = userObject2?.city ? userObject2?.city : ''
+
+        const lastDate = client.date.split('T')
+        const d = new Date(lastDate[0]);
+				const year = d.getFullYear();
+				const month = String(d.getMonth()+1).padStart(2, "0");
+				const day = String(d.getDate()).padStart(2, "0");
+				const newDateActivity = `${day}.${month}.${year}`
+
+        const newClientName = client.name.includes("|") ? client.name.split(" | ")[1] : client.name
+
+        const allMessages = messages.length //всего сообщений
+        const fromAdmin = messages.filter(el => el.senderId === chatAdminId);
+        const messagesUsers = messages.filter(el => el.senderId === client.chatId);
+
+        const arr = clients.filter(el => el.message === '');
+        setNewClients(arr)
+
+        const arr2 = clients.filter(el => el.message !== '');
+        setOldClients(arr2)
+        
+        const newObj = {
+          avatar: client.avatar,
+          user: {
+            name: newClientName,
+            new: true,
+            registered: '01.01.2023',
+          },
+          TG_ID: client.chatId,
+          city: companyCity,
+          company: companyName ? companyName : '',
+          phone: userObject?.phone,
+          usage: {
+            value: Math.round(messagesUsers.length * 100 / (allMessages - fromAdmin.length)), 
+            period: '01.04.2023 - ' + newDateActivity,
+            color: 'success',
+          },
+          activity: newDateActivity,
+        }
+
+        arrClients.push(newObj)
+      })
+
+      const filteredClients = [...arrClients].filter((el) => el.TG_ID !== chatAdminId); //без админского пользователя  
+      const clientSort = [...filteredClients].sort((a, b) => {       
+				return b.usage.value-a.usage.value  //сортировка по убывающей активности  
+			}) 
+      console.log('userbots: ', clientSort)  
+      setContacts(clientSort)  
+      
+      setTimeout(() => {
+        setLoading(false)
+      }, "6000")
+    }
+    
+    fetchData();
+    
+  }, [clients]);
+//---------------------------------------------------------------------------------------------
+//get Projects
+  useEffect(() => {
+    const arrProjects = []
+
+    const fetchData = async () => {
+			//let response = await getProjects();
+      projs.map(async (project) => {
+        const newProject = {
+					id: project.id,
+					name: project.title,
+					start: project.time_start,
+          created: project.time_created,
+          teh: project.teh,
+          manager: project.manager,
+          company: project.company,
+				}
+        arrProjects.push(newProject)
+      })
+
+      setProjects(arrProjects) 
+    }
+
+    fetchData();
+    
+  },[projs])
+
   return (
     <div className='dark-theme'>
       <AppSidebar />
@@ -53,10 +161,140 @@ const Admin = () => {
         <div className="body flex-grow-1 px-3">
 
             <CContainer lg>
-              <Suspense fallback={<CSpinner color="primary" />}>
-                <h2>Панель управления</h2>
-                <h5>Раздел находится в разработке</h5>
-              </Suspense>
+                <Suspense fallback={<CSpinner color="primary" />}>
+
+                <>
+                <WidgetsDropdown users={clients.length-1} projects={projects.length} companys={comps.length} />
+
+                <CRow>
+                  <CCol xs>
+                    <CCard className="mb-4">
+                      <CCardHeader>Пользователи бота ({clients.length - 1})</CCardHeader>
+                      <CCardBody>
+                        <CRow>
+                          <CCol xs={12} md={6} xl={6}>
+                            <CRow>
+                              <CCol sm={6}>
+                                <div className="border-start border-start-4 border-start-info py-1 px-3">
+                                  <div className="text-medium-emphasis small">Новые клиенты</div>
+                                  <div className="fs-5 fw-semibold">{newClients.length}</div>
+                                </div>
+                              </CCol>
+                              <CCol sm={6}>
+                                <div className="border-start border-start-4 border-start-danger py-1 px-3 mb-3">
+                                  <div className="text-medium-emphasis small">Постоянные клиенты</div>
+                                  <div className="fs-5 fw-semibold">{oldClients.length-1}</div>
+                                </div>
+                              </CCol>
+                            </CRow>
+                          </CCol>
+
+                          <CCol xs={12} md={6} xl={6}>
+                            <CRow>
+                              <CCol sm={6}>
+                                <div className="border-start border-start-4 border-start-warning py-1 px-3 mb-3">
+                                  <div className="text-medium-emphasis small">Просмотры</div>
+                                  <div className="fs-5 fw-semibold">-</div>
+                                </div>
+                              </CCol>
+                              <CCol sm={6}>
+                                <div className="border-start border-start-4 border-start-success py-1 px-3 mb-3">
+                                  <div className="text-medium-emphasis small">Другое</div>
+                                  <div className="fs-5 fw-semibold">-</div>
+                                </div>
+                              </CCol>
+                            </CRow>
+
+                            <div className="mb-5"></div>
+
+                          </CCol>
+                        </CRow>
+
+                      {loading ? 
+                                  
+                        <CSpinner/> :
+
+                        <CTable align="middle" className="mb-0 border" hover responsive>
+                          <CTableHead className='table-dark'>
+                            <CTableRow>
+                              <CTableHeaderCell style={{width: '30px'}}>№</CTableHeaderCell>
+                              <CTableHeaderCell className="text-center" style={{width: '100px'}}>
+                                <CIcon icon={cilPeople} />
+                              </CTableHeaderCell>
+                              <CTableHeaderCell style={{width: '160px'}}>Пользователь</CTableHeaderCell>                             
+                              <CTableHeaderCell className="text-center" style={{width: '160px'}}>Организация</CTableHeaderCell>
+                              <CTableHeaderCell className="text-center" style={{width: '160px'}}>Телефон</CTableHeaderCell>
+                              <CTableHeaderCell className="text-center" style={{width: '80px'}}>Город</CTableHeaderCell>
+                              <CTableHeaderCell className="text-center" style={{width: '100px'}}>TG ID</CTableHeaderCell>
+                              <CTableHeaderCell style={{width: '100px'}}>Использование</CTableHeaderCell>
+                              <CTableHeaderCell style={{width: '100px'}}>Активность</CTableHeaderCell>
+                              {/* <CTableHeaderCell>Управление</CTableHeaderCell> */}
+                            </CTableRow>
+                          </CTableHead>
+                          <CTableBody>
+                            {contacts.map((item, index) => (
+                              <CTableRow v-for="item in tableItems" key={index}>
+                                <CTableDataCell className="text-center">
+                                  {index+1}
+                                </CTableDataCell>
+                                <CTableDataCell className="text-center">
+                                    <CAvatar size="md" src={item.avatar ? host + item.avatar : avatar2} alt='' />
+                                </CTableDataCell>
+                                <CTableDataCell>
+                                  <div>{item.user.name}</div>
+                                  <div className="small text-medium-emphasis">
+                                    {/* <span>{item.user.new ? 'Новый' : 'Recurring'}</span> | Регистрация:{' '}
+                                    {item.user.registered} */}
+                                  </div>
+                                </CTableDataCell>
+                                <CTableDataCell className="text-center">
+                                  {item.company ? <div>{item.company}</div> : ''}
+                                </CTableDataCell>
+                                <CTableDataCell className="text-center">
+                                  <div>{item.phone}</div>
+                                </CTableDataCell>
+                                <CTableDataCell className="text-center">
+                                  <div>{item.city}</div>
+                                </CTableDataCell>
+                                <CTableDataCell className="text-center">
+                                  <div>{item.TG_ID}</div>
+                                </CTableDataCell>
+                                <CTableDataCell>
+                                  <div className="clearfix">
+                                    <div className="float-start">
+                                      <strong>{item.usage.value}%</strong>
+                                    </div>
+                                    <div className="float-end">
+                                      <small className="text-medium-emphasis">{item.usage.period}</small>
+                                    </div>
+                                  </div>
+                                  <CProgress thin color={item.usage.color} value={item.usage.value} />
+                                </CTableDataCell>
+                                <CTableDataCell>
+                                  <div className="small text-medium-emphasis">Последний вход</div>
+                                  <strong>{item.activity}</strong>
+                                </CTableDataCell>
+                                {/* <CTableDataCell>
+                                  <CButton color="light">
+                                    <img src={pencilIcon} alt='' width='15px'/>
+                                  </CButton>
+                                  &nbsp;
+                                  <CButton color="light">
+                                    <img src={deleteIcon} alt='' width='10px' />
+                                  </CButton>
+                                </CTableDataCell> */}
+                              </CTableRow>
+                            ))}
+                          </CTableBody>
+                        </CTable>
+                      }
+                      </CCardBody>
+                    </CCard>
+                  </CCol>
+                </CRow>
+              </>
+
+                </Suspense>
             </CContainer>
 
         </div>
