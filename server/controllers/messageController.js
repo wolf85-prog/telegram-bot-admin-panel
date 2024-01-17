9ed16ddbd20e4d9f39eb9cf13eaada66a295cc5c
Sergey <seregaland@yandex.ru>
2024-01-17 09:51:47 +0300

    open 100 messages

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -57,7 +57,7 @@ class MessageController {
                 order: [
                     ['id', 'ASC'], //DESC
                 ],
-                offset: count > 50 ? count - 50 : 0,
+                offset: count > 150 ? count - 150 : 0,
                 //limit : 50,
             })
             return res.status(200).json(messages);
