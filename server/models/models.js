d8739e2b68fa8ea2c8fe8dca27b8c70c9a8d008a
Sergey <seregaland@yandex.ru>
2024-02-20 16:36:39 +0300

    fix

--- server/models/models.js
+++ server/models/models.js
@@ -88,7 +88,7 @@ const Report = sequelize.define('report', {
     delivered: {type: DataTypes.BOOLEAN}, //доставлено
 })
 
-const SoundNotif = sequelize.define('soundnotif', {
+const SoundNotif = sequelize.define('soundnotifs', {
     id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true}, 
     name: {type: DataTypes.STRING},  //название проекта
     text: {type: DataTypes.STRING}, //текст сообщения;
