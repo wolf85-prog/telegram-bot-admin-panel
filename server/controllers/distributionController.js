bf08b304db3d2cf4e9f3a6ddff13c4ba8a0adf33
seregey gornih <you@example.com>
2024-02-28 10:47:27 +0300

    fix arrUser

--- server/controllers/distributionController.js
+++ server/controllers/distributionController.js
@@ -261,7 +261,8 @@ class DistributionController {
     //send message
     async sendDistribW(req, res) {
         const {id} = req.params  
-
+        let arrUsers = []
+        
         try {
             let exist=await Distributionw.findOne( {where: {id: id}} )
             
