30afc4cb94a7e6dce26ee753c6b4dddfc30e2ed1
seregey gornih <you@example.com>
2024-04-30 11:56:07 +0300

    fix

--- server/routes/route.js
+++ server/routes/route.js
@@ -33,7 +33,7 @@ const { uploadFile, getImage, sendNarush } = require( "../controllers/fileContro
 const { getUserWorkers, getUserWorker, editUserWorker} = require('../controllers/wuserbotController')
 const { newMessageWorker, delMessageWorker, getMessagesWorker, getMessagesWorker2, getAllMessagesWorker, getMessagesWorkerCount } = require('../controllers/wmessageController')
 const { newConversationWorker, getConversationWorker, getConversationsW } = require('../controllers/wconversationController')
-const { getWorkers, getWorkersCount, getWorker, editWorker, blockWorker} = require('../controllers/workersController')
+const { getWorkers, getWorkersCount, getWorker, editWorker, blockWorker, getCanceled} = require('../controllers/workersController')
 const { newPretendent, getPretendent, getPretendentId, getAllPretendent, getAllPretendentCount } = require('../controllers/pretendentController')
 
 const { newPlan, getPlan, addTimer } = require('../controllers/planController')
