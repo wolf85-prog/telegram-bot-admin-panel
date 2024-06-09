82c0154a05b518f123b364961733f4f4431543b1
seregey gornih <you@example.com>
2024-06-09 08:20:47 +0300

    fix

--- socket/socket_app.js
+++ socket/socket_app.js
@@ -149,7 +149,7 @@ io.on("connection", (socket) => {
     //------------------------------------------------------------------
     //send and get message in workers
     socket.on("sendNotif", ({task, tg_id, fio, sity, year_of_birth, rating, projects, specialities, 
-        comtags, foto, phone, workers_update, processUpdateD, telegram_id, srm_id, chat_link})=>{
+        comtags, foto, phone, workers_update, processUpdateD})=>{
         io.emit("getNotif", {
             task,
             tg_id,
@@ -164,9 +164,6 @@ io.on("connection", (socket) => {
             phone,
             workers_update,
             processUpdateD,
-            telegram_id, 
-            srm_id, 
-            chat_link,
         })
     })  
     
