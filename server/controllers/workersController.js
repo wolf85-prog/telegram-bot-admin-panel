df48ed26fb95b26baf001da3e7390d0894a4f0a9
Sergey Gornih <seregaland@yandex.ru>
2024-08-12 14:19:40 +0300

    fix

--- server/controllers/workersController.js
+++ server/controllers/workersController.js
@@ -177,7 +177,7 @@ class WorkersController {
                     j++       
                     //await delay(1000) // 10 сек  
                     new Promise((resolve) => {
-                        setTimeout(resolve, 3000);
+                        setTimeout(resolve, 2000*j);
                     });           
                 }
 
