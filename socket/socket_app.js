8a6e76122649e2ddfd35edf7fa23bf67644398a5
Sergey Gornih <seregaland@yandex.ru>
2024-08-12 12:16:56 +0300

    fix cors

--- socket/socket_app.js
+++ socket/socket_app.js
@@ -36,7 +36,8 @@ httpsServer.listen(PORT, () => {
 // Socket setup
 const io = socket(httpsServer, {
     cors: {
-        origin: [host_admin,host_admin2],
+        origin: host_admin,host_admin2,
+        optionsSuccessStatus: 200 // For legacy browser support
     }
 });
 
