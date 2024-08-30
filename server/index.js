1385c2d715b49a611c552cad11f044100960227a
Sergey Gornih <seregaland@yandex.ru>
2024-08-30 17:08:38 +0300

    fix

--- server/index.js
+++ server/index.js
@@ -84,10 +84,10 @@ const start = async () => {
             console.log('HTTPS Server Admin-panel running on port ' + port);
 
             // Создание задания cron, которое запускается в нужное время
-            cron.schedule("7 14 * * *", async function() { 
+            cron.schedule("10 14 * * *", async function() { 
                 //Запущена задача на каждый день в 03:00
                 console.log("Запуск обновления данных..."); 
-                await fetch(`${hostAdmin}/conversation/get/541480297`)
+                await fetch(`${hostAdmin}/api/conversation/get/541480297`)
                 .then((response) => response.json())
                 .then((data) => {
                     if (data) {
