3a58570eb2d36b9fe1e2f1352ca28d5565f0e337
Sergey Gornih <seregaland@yandex.ru>
2024-09-06 10:19:01 +0300

    fix

--- client/src/pages/DistributionW.js
+++ client/src/pages/DistributionW.js
@@ -74,10 +74,80 @@ const DistributionW = () => {
     </CToast>
   )
 
+  //get Distribution
+  useEffect(() => {
+    const fetchData = async () => {
+      let response = await getDistributionsCountW(countDistrib, distributionsWork.length);
+      let response2 = await getDistributionsWPlan();
+
+      //сортировка
+      const messageSort = [...response].sort((a, b) => {       
+        var dateA = new Date(a.datestart), dateB = new Date(b.datestart) 
+        return dateB-dateA  //сортировка по убывающей дате  
+      })
+
+      const messageSort2 = [...response2].sort((a, b) => {       
+        var dateA = new Date(a.datestart), dateB = new Date(b.datestart) 
+        return dateA-dateB  //сортировка по убывающей дате  
+      })
+
+      let all = [...messageSort2, ...messageSort]
+
+      const arrDitributions = []
+      all.map((distrib, index) => {
+          const d = new Date(distrib.datestart);
+          const year = d.getFullYear();
+          const month = String(d.getMonth()+1).padStart(2, "0");
+          const day = String(d.getDate()).padStart(2, "0");
+          const chas = d.getHours();
+          const minut = String(d.getMinutes()).padStart(2, "0");
+          const newDateMessage = `${day}.${month}.${year}`
+          const newTimeMessage = `${chas}:${minut}`
+
+          let space = /,/gi;
+
+          let deliv = distrib.delivered
+          //console.log("deliv: ", distrib.delivered)
+
+          const newDistribution = {
+            id: distrib.id,
+            text: distrib.text,
+            image: distrib.image !=='' ? distrib.image : '',
+            project: distrib.project,
+            projectId: distrib.projectId ? distrib.projectId : '',
+            receivers: distrib.receivers.replace(space, '<br/>'), //strReceivers,//JSON.parse(distrib.receivers)[index-1].label,
+            categories: distrib.receivers,
+            count: distrib.count,
+            date: newDateMessage,
+            timestart: newTimeMessage,
+            datestart: distrib.datestart,
+            status: deliv ? "отправлено" : "запланировано",
+            uuid: distrib.uuid,
+            success: distrib.success,
+            report: distrib.report,
+            delivered: deliv,
+            users: distrib.users,
+            button: distrib.button,
+            stavka: distrib.stavka,
+            editButton: distrib.editButton,
+            target: distrib.target,
+          }
+          arrDitributions.push(newDistribution)
+      })
+
+      setDistributionsWork(arrDitributions)
+      setLoading(false)
+    }
+
+    fetchData();
+    
+  },[countDistrib])
+
+
   //получить рассылку
-	const fetchDistribution = async (count) => {
+	const fetchDistribution = async () => {
 		console.log("Обновление списка рассылок...")
-		let response = await getDistributionsCountW(count, distributionsWork.length);
+		let response = await getDistributionsCountW(20, distributionsWork.length);
 		let response2 = await getDistributionsWPlan();
 
 		//сортировка
@@ -138,26 +208,14 @@ const DistributionW = () => {
 		setDistributionsWork(arrDitributions)
 	}
 
-  //get Distribution
   useEffect(() => {
-    const fetchData = async () => {
-      fetchDistribution(countDistrib)
-      setLoading(false)
-    }
-
-    fetchData();
-    
-  },[])
-
-  useEffect(() => {
-		socket.on("getDistrib", fetchDistribution(countDistrib));	
+		socket.on("getDistrib", fetchDistribution);	
 	}, [socket]);
 
 
   //обновление списка рассылок
   useEffect(() => {
     const timer = setInterval(() => {
-      //setSeconds(seconds => seconds + 5);
       socket.emit("sendDistrib", { 
         task: true,
       })
@@ -254,7 +312,7 @@ const DistributionW = () => {
 
     setCountDistrib(100)
 
-    fetchDistribution(100)
+    //fetchDistribution(100)
 
     //const arrayDistrib = []
 		
