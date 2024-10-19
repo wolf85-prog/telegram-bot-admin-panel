563d71ff65da50393e4df206c2a4ec7b384427ac
Sergey Gornih <seregaland@yandex.ru>
2024-10-19 18:23:29 +0300

    fix

--- client/src/components/Select2/Select2.js
+++ client/src/components/Select2/Select2.js
@@ -9,7 +9,7 @@ const Select2 = ({menuShow, setMenuShow, selected}) => {
             onClick={()=> setMenuShow(!menuShow)}
         >
             <span style={{color: `${selected.color}`}}>
-                {selected.name}
+                {selected.name && selected.name.length > 25 ? selected.name.substr(0, 25) + '...' : selected.name}
             </span>
             {/* <div className={`${cl.caret} ${menuShow && cl.caretRotate}`}></div> */}
         </div>
