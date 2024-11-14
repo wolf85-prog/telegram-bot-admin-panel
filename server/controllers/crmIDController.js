61cd9acb285322885414a1cd1d9a38945dddd377
Sergey <seregaland@yandex.ru>
2024-11-14 17:03:57 +0300

    fix

--- server/controllers/crmIDController.js
+++ server/controllers/crmIDController.js
@@ -22,9 +22,11 @@ class crmIDController {
         try {     
 
             const crm = await sequelize.query("SELECT nextval('crm4_id')");
+
+            const res = crm[0][0].nextval
             
 
-            return res.status(200).json(crm);
+            return res.status(200).json(res);
         } catch (error) {
             return res.status(500).json(error.message);
         }
