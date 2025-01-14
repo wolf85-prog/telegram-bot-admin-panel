806a2c9601ccf9722a9ad206dbd46c9127bebc3c
Sergey <seregaland@yandex.ru>
2025-01-14 09:10:02 +0300

    fix specData server

--- server/data/specData.js
+++ server/data/specData.js
@@ -11,6 +11,7 @@ module.exports = {
                {id: 4, name: 'Backline',},
                {id: 5, name: 'Roadie',},
                {id: 6, name: 'Техник по звуку',},
+               {id: 7, name: 'Микрофонный техник'},
            ]
        },
        {
@@ -18,10 +19,11 @@ module.exports = {
            name: 'LIGHT',
            icon: 'Light',
            models: [
-               {id: 1, name: 'Художник по свету',},
-               {id: 2, name: 'Оператор световой пушки',},
-               {id: 3, name: 'Гафер',},
-               {id: 4, name: 'Техник по свету',},
+                {id: 1, name: 'Художник по свету',},
+                {id: 2, name: 'Оператор световой пушки',},
+                {id: 3, name: 'Гафер',},
+                {id: 4, name: 'Техник по свету',},
+                {id: 5, name: 'Электрик',},
            ]
        },
        {
@@ -29,14 +31,14 @@ module.exports = {
            name: 'VIDEO',
            icon: 'Video',
            models: [
-               {id: 1, name: 'Инженер VMix',},
-               {id: 2, name: 'Инженер Resolume',},
-               {id: 3, name: 'Оператор Zoom',},
-               {id: 4, name: 'Оператор [cameraman]',},
-               {id: 5, name: 'Оператор-постановщик',},
-               {id: 6, name: 'Режиссер эфиров',},
-               {id: 7, name: 'Техник монтажа',},
-               {id: 8, name: 'IT-специалист',},  
+                {id: 1, name: 'Инженер VMix',},
+                {id: 2, name: 'Инженер Resolume',},
+                {id: 3, name: 'Оператор',},
+                {id: 4, name: 'Оператор Zoom-конференций',},
+                {id: 5, name: 'Оператор Action-съемки',},
+                {id: 6, name: 'Оператор-постановщик',},
+                {id: 7, name: 'Режиссер эфиров',},
+                {id: 8, name: 'Техник по монтажу экранов',},   
            ]
        },
        {
@@ -44,38 +46,54 @@ module.exports = {
             name: 'PHOTO',
             icon: 'Photo',
             models: [
-                {id: 1, name: 'Свадебная съемка',},
+                {id: 1, name: 'Документальная съемка',},
                 {id: 2, name: 'Репортажная съемка',},
-                {id: 3, name: 'Портретная съемка',},
-                {id: 4, name: 'Предметная съемка',},
-                {id: 5, name: 'Документальная съемка',},
+                {id: 3, name: 'Свадебная съемка',}, 
+                {id: 4, name: 'Портретная съемка',},
+                {id: 5, name: 'Предметная съемка',},
             ]
         },
         {
             id: 5,
-            name: 'PROMO',
-            icon: 'Promo',
+            name: 'KINO',
+            icon: 'Kino',
             models: [
-                {id: 1, name: 'Модель',},
-                {id: 2, name: 'Актер',},
-                {id: 3, name: 'Промоутер',},
-                {id: 4, name: 'Гример',},
+                {id: 1, name: 'Режиссер', color: '#fff'},
+                {id: 2, name: 'Оператор-постановщик', color: '#fff'},
+                {id: 3, name: 'Стедикам', color: '#fff'},
+                {id: 4, name: 'Оператор крана', color: '#fff'},
+                {id: 5, name: 'Оператор Action-съемки', color: '#fff'},
+                {id: 6, name: 'Гафер', color: '#fff'},
+                {id: 7, name: 'Актер', color: '#fff'},
+                {id: 8, name: 'Гример', color: '#fff'},
+                {id: 9, name: 'Костюмер', color: '#fff'},
             ]
         },
         {
             id: 6,
+            name: 'PROMO',
+            icon: 'Promo',
+            models: [
+                {id: 1, name: 'Модель',},
+                {id: 2, name: 'Промоутер',},         
+                {id: 3, name: 'Визажист',},
+                {id: 4, name: 'Костюмер',},
+            ]
+        },
+        {
+            id: 7,
             name: 'CATERING',
             icon: 'Catering',
             models: [
-                {id: 1, name: 'Официант',},
-                {id: 2, name: 'Бармен',},
-                {id: 3, name: 'Повар',},
-                {id: 4, name: 'Хостес',},
-                {id: 5, name: 'Банкетный менеджер',},
+                {id: 1, name: 'Банкетный менеджер',},  
+                {id: 2, name: 'Повар',}, 
+                {id: 3, name: 'Хостес',},  
+                {id: 4, name: 'Бармен',},
+                {id: 5, name: 'Официант',}, 
             ]
         }, 
         {
-            id: 7,
+            id: 8,
             name: 'STAGEHANDS',
             icon: 'Stagehands',
             models: [
@@ -83,29 +101,42 @@ module.exports = {
             ]
         },
        {
-           id: 8,
+           id: 9,
            name: 'RIGGERS',
            icon: 'Riggers \/ Ground',
            models: [
-               {id: 1, name: 'Верхний Риггер',},
-               {id: 2, name: 'Нижний Риггер',},
-               {id: 3, name: 'Монтажник [Ground]',},
-               {id: 4, name: 'Декоратор',},
-           ]
-       },
-       {
-           id: 9,
-           name: 'TRUCKS',
-           icon: 'Trucks',
-           models: [
-               {id: 1, name: 'C личным ТС [B\/C]',},
-               {id: 2, name: 'Без личного ТС [B\/C]',},
-               {id: 3, name: 'С гидролифтом',},
-               {id: 4, name: 'Без гидролифта',},
+                {id: 1, name: 'Верхний Риггер',},
+                {id: 2, name: 'Нижний Риггер',},
+                {id: 3, name: 'Монтаж конструктива сцены',},
+                {id: 4, name: 'Монтаж шатров',},
            ]
        },
        {
             id: 10,
+            name: 'DECOR',
+            icon: 'Decor',
+            models: [
+                {id: 1, name: 'Декоратор',},
+                {id: 2, name: 'Флорист оформитель',},
+                {id: 3, name: 'Монтаж мебели',},
+                {id: 3, name: 'Монтаж выставочных стендов',},
+                {id: 3, name: 'Напольные покрытия',},
+            ]
+        },
+       {
+           id: 11,
+           name: 'TRUCKS',
+           icon: 'Trucks',
+           models: [
+                {id: 1, name: 'С гидролифтом',},
+                {id: 2, name: 'Без гидролифта',},
+                {id: 3, name: 'C личным ТС [B\/C]',},
+                {id: 4, name: 'Без личного ТС [B\/C]',},
+                {id: 5, name: 'Мотокурьер',},
+           ]
+       },
+       {
+            id: 12,
             name: 'PARTY',
             icon: 'Party',
             models: [
@@ -115,32 +146,33 @@ module.exports = {
                 {id: 4, name: 'Ведущий',},
                 {id: 5, name: 'Певец \/ певица',},
                 {id: 6, name: 'Артист оригинального жанра',},
-                {id: 7, name: 'Go-Go',},
+                {id: 7, name: 'Фаир-шоу',},
+                {id: 8, name: 'Go-Go',},
             ]
         },
         {
-            id: 11,
+            id: 13,
             name: 'GAMES',
             icon: 'Games',
             models: [
-                {id: 1, name: 'Квесты',},
-                {id: 2, name: 'Квизы',},
-                {id: 3, name: 'Аниматоры',},
-                {id: 4, name: 'Настольные игры \/ игровые автоматы',},
-                {id: 5, name: 'Пневмокостюмы \/ ростовые куклы',},
+                {id: 1, name: 'Квест',},
+                {id: 2, name: 'Квиз',},
+                {id: 3, name: 'Аниматор',},
+                {id: 4, name: 'Пневмокостюм \/ ростовая кукла',},
+                {id: 5, name: 'Настольные игры \/ игровые автоматы',},
                 {id: 6, name: 'Активности \/ аттракционы',},
             ]
         },
         {
-           id: 12,
+           id: 14,
            name: 'PRODUCTION',
            icon: 'Production',
            models: [
-               {id: 1, name: 'Мероприятие под ключ',},
-               {id: 2, name: 'Отдельные технические задачи',},
+                {id: 1, name: 'Мероприятие под ключ',},
+                {id: 2, name: 'Отдельные технические задачи',},
+                {id: 3, name: 'Event-менеджер',},
            ]
         },  
    ]
    
 }
-   
