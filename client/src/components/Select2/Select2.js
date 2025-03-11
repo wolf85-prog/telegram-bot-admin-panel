18c70bccfe5aedb7e5b318b848386813828ebbf3
Sergey <seregaland@yandex.ru>
2025-03-11 09:30:23 +0300

    fix color

--- client/src/components/Select2/Select2.js
+++ client/src/components/Select2/Select2.js
@@ -26,7 +26,9 @@ const Select2 = ({menuShow, setMenuShow, clearShow, setClearShow, selected, el})
             className={`${cl.select} ${menuShow && cl.selectClicked}`}
             onClick={clickSelect}
         >
-            <span style={{color: `${element?.name === 'Передумал' ? 'red' : element?.color}`}}>
+            <span style={{color: `${element?.name === 'Отмена' || 
+                element?.name === 'Фальшстарт' ||
+                element?.name === 'Передумал' ? 'red' : element?.color}`}}>
                 {element?.name && element?.name.length > 25 ? element?.name.substr(0, 25) + '...' : element?.name}
             </span>
             
