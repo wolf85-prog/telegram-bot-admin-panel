3dbd2150335dc0d6418510e10021400255ef5df3
Sergey Gornih <seregaland@yandex.ru>
2025-04-21 12:16:11 +0300

    fix

--- client/src/data/startData.js
+++ client/src/data/startData.js
@@ -1,13 +1,13 @@
 const startData = [
   // {value: 0, label: 'Выбери специфику',},
 
-  {value: 1, label: "Проект 180 минут", color: '#1E90FF'},
-  {value: 2, label: "Проект 150 минут", color: '#1E90FF'}, 
-  {value: 3, label: 'Проект 120 минут', color: '#1E90FF'},
-  {value: 4, label: "Проект 90 минут", color: '#1E90FF'},
-  {value: 5, label: "Проект 60 минут", color: '#1E90FF'},
-  {value: 6, label: "Проект 30 минут", color: '#1E90FF'},
-  {value: 7, label: "Проект 15 минут", color: '#1E90FF'},
+  {value: 1, label: "180 минут", color: '#1E90FF'},
+  {value: 2, label: "150 минут", color: '#1E90FF'}, 
+  {value: 3, label: '120 минут', color: '#1E90FF'},
+  {value: 4, label: "90 минут", color: '#1E90FF'},
+  {value: 5, label: "60 минут", color: '#1E90FF'},
+  {value: 6, label: "30 минут", color: '#1E90FF'},
+  {value: 7, label: "15 минут", color: '#1E90FF'},
 ];
 
 export default startData;
