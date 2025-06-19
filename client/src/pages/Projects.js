5598783546232a5ea0574d6ee9479fffbc35e63c
tembot <tembot@DESKTOP-GBOTIM4>
2025-06-19 16:39:58 +0300

    fix worker report url

--- client/src/pages/Projects.js
+++ client/src/pages/Projects.js
@@ -1432,8 +1432,8 @@ ${loc.url}`
     const keyboard = JSON.stringify({
 			inline_keyboard: [
 				[
-					{"text": "XLSX", url:'https://proj.uley.team/upload/poster_akkreditacija.xlsx'},
-          {"text": "PDF", url:'https://proj.uley.team/upload/poster_akkreditacija.pdf'},
+					{"text": "XLSX", url:`https://testtm.uley.team/files/${item.url}.xlsx`},
+          {"text": "PDF", url:`https://testtm.uley.team/files/${item.url}.pdf`},
 				],
 			]
 		});
